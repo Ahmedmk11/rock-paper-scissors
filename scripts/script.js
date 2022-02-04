1b7e1faa3944ffb127806097957d97803e94814b
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-02-04 11:33:35 +0200

    Fixed game logic for UI

--- scripts/script.js
+++ scripts/script.js
@@ -2,60 +2,75 @@ function computerPlay(){
     let computerMove = Math.floor(3 * Math.random());
     switch (computerMove){
         case 0:
-            result = "rock";
+            result = "Rock";
             break;
         case 1:
-            result = "paper"
+            result = "Paper";
             break;
         case 2:
-            result = "scissors"
+            result = "Scissors";
             break;
     }
     return result;
 }
+
 function gameRound(playerSelection, computerSelection){
-    if(computerSelection == "rock" && playerSelection.toLowerCase() == "rock" || computerSelection == "paper" && playerSelection.toLowerCase() == "paper" || computerSelection == "scissors" && playerSelection.toLowerCase() == "scissors"){
-        console.log("It's a Tie!");
-        return 0;
-    }else if(computerSelection == "rock" && playerSelection.toLowerCase() == "scissors" || computerSelection == "paper" && playerSelection.toLowerCase() == "rock" || computerSelection == "scissors" && playerSelection.toLowerCase() == "paper"){
-        console.log(`You Lose! ${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1)}. :(`);
-        return -1;
+    console.log(`Round: ${stats[2]} \nPlayer: ${stats[0]} \nComputer: ${stats[1]}`);
+    const results = document.querySelector('#results-container');
+
+    if(computerSelection == "Rock" && playerSelection == "Rock" || 
+            computerSelection == "Paper" && playerSelection == "Paper" || 
+            computerSelection == "Scissors" && playerSelection == "Scissors"){
+
+        results.textContent = "It's a Tie!";
+        statsTracking(0,0,1);
+
+    }else if(computerSelection == "Rock" && playerSelection == "Scissors" ||
+                computerSelection == "Paper" && playerSelection == "Rock" ||
+                computerSelection == "Scissors" && playerSelection == "Paper"){
+
+        results.textContent = `You Lose! ${computerSelection} beats ${playerSelection}. :(`;
+        statsTracking(0,1,0);
+
     }else{
-        console.log(`You Win! ${playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1)}. :)`);
-        return 1;
+        results.textContent = `You Win! ${playerSelection} beats ${computerSelection}. :)`;        
+        statsTracking(1,0,0);
+    }
+    if(isGameOver()){
+        const final = document.querySelector('#results-container');
+        if(stats[0] > stats[1]){
+            final.textContent = "You Won The Game!";
+        }else{
+            final.textContent = "You Lost The Game";
+        } 
     }
 }
+
+function isGameOver(){
+    return stats[0] == 5 || stats[1] == 5;
+}
+
 function mainGame(){
-    let roundNum = 0;
-    let playerWins = 0;
-    let computerWins = 0;
-    while(playerWins < 5 && computerWins < 5){
-        let invalidInput = true; 
-        while(invalidInput){
-            playerInput = window.prompt("Rock, Paper or Scissors?"); 
-            if(playerInput == "rock" || playerInput == "paper" || playerInput == "scissors"){
-                invalidInput = false;
-            }else{
-                console.log("wrong!");
-            }
-        }
-        let thisRound = gameRound(playerInput, computerPlay());
-        if (thisRound > 0){
-            playerWins ++;
-            roundNum ++;
-        }else if (thisRound < 0){
-            computerWins ++;
-            roundNum ++;
-        }else{
-            roundNum ++;
-        }
-        console.log(`Round: ${roundNum} \nPlayer: ${playerWins} \nComputer: ${computerWins}`);
-    }
-    if(playerWins > computerWins){
-        console.log("You Won!");
-    }else{
-        console.log("You Lose!");
-    }
     
 }
-mainGame();
+function statsTracking(win,lose,tie){
+    if (win == 1){
+        stats[0] += 1;
+        stats[2] += 1;
+    }else if(lose == 1){
+        stats[1] += 1;
+        stats[2] += 1;
+    }else if(tie == 1){
+        stats[2] += 1;
+    }
+}
+
+const start = document.querySelector('#start-button');
+start.addEventListener('click',mainGame);
+const stats = [0,0,0];
+
+
+const btns = document.querySelectorAll(".buttons");
+btns.forEach(btn => btn.addEventListener("click", () => {
+    (gameRound(btn.id,computerPlay())) 
+}));
